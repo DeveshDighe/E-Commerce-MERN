c5cc017ebddf46e556083fd6c88a0dc61e54fb57
DeveshDighe <deveshdighe30@gmail.com>
2024-06-20 22:57:56 +0530

    updates

--- FrontEnd/src/State/Cart/Action.js
+++ FrontEnd/src/State/Cart/Action.js
@@ -20,7 +20,7 @@ export const addItemToCart = (reqData) => async (dispatch) => {
         const { data } = await api.put('/api/cart/add', reqData)
         console.log('This is aaa', data);
         dispatch(getCart())
-        dispatch({ type: ADD_ITEM_TO_CART_SUCCESS, payload: data })
+        // dispatch({ type: ADD_ITEM_TO_CART_SUCCESS, payload: data })
 
     } catch (error) {
         dispatch({ type: ADD_ITEM_TO_CART_FAILURE, payload: error.message })
