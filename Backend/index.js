42c423f5eb62ccf8a86a94219a2dc068e8e7c1f3
DeveshDighe <deveshdighe30@gmail.com>
2024-06-20 19:56:32 +0530

    update

--- Backend/index.js
+++ Backend/index.js
@@ -6,7 +6,7 @@ const app = express()
 
 app.use(express.json())
 const corsOptions = {
-    origin: 'https://e-commerce-mern-project-devesh-dighe.vercel.app', // Explicitly allow your frontend domain
+    origin: 'https://e-commerce-mern-project-devesh-dighe.vercel.app/', // Explicitly allow your frontend domain
     methods: 'GET,POST,PUT,DELETE', // Specify allowed methods as needed
     credentials: true, // If your frontend needs to send cookies or credentials with the request
     allowedHeaders: 'Content-Type,Authorization', // Specify allowed headers
